7380253de7f3364d621d84ee573aabfda315df5f
nidhi <nidhi7485@gmail.com>
2022-11-08 23:41:28 +0530

    users completed with authentication

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 
+require('dotenv').config()
 const app = express()
 
 const port = process.env.PORT || 3000
+
+// db
+const connectDB = require('./db/connect')
 
 // router
 const foodApiRouter = require('./routes/foodApiRoute')
@@ -14,6 +18,7 @@ app.use('/', foodApiRouter)
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`server is running on port: ${port}...`))
   } catch (error) {
     console.log(error)
--- controller/usersController.js
+++ controller/usersController.js
@@ -1,6 +1,11 @@
+const User = require('../models/User')
+
 const registerUser = async (req, res) => {
   try {
-    res.status(200).json({ msg: 'register' })
+    // console.log(req.body)
+    const user = await User.create(req.body)
+    const token = user.creatjwt()
+    res.status(200).json({ msg: 'user added successfully', user, token })
   } catch (error) {
     console.log(error)
   }
@@ -8,7 +13,21 @@ const registerUser = async (req, res) => {
 
 const logIn = async (req, res) => {
   try {
-    res.status(200).json({ msg: 'successfully signin' })
+    const { phone, password } = req.body
+    // console.log(phone, password)
+    if (!phone || !password) {
+      return res.send('please provide email or password')
+    }
+    const user = await User.findOne({ phone })
+    if (!user) {
+      return res.send('invalid credential')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+      return res.send('invalid credential')
+    }
+    const token = user.creatjwt()
+    res.status(200).json({ msg: 'successfully login', user, token })
   } catch (error) {
     console.log(error)
   }
